e4da776c58690129ae8a16caeede805cc7183ea9
Pragya Shree <pragyashree1667@gmail.com>
2026-08-02 14:12:06 +0530

    fix: allow all Vercel deployments in CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -43,7 +43,8 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=settings.CORS_ORIGINS,
-    allow_origin_regex=r"https?://(localhost|127\.0\.0\.1)(:\d+)?",
+    # allow_origin_regex=r"https?://(localhost|127\.0\.0\.1)(:\d+)?",
+    allow_origin_regex=r"https://.*\.vercel\.app|https?://(localhost|127\.0\.0\.1)(:\d+)?",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
